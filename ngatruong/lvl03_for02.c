2cad7876db0fa11e281451004fc20deaf3702cc7
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-20 00:02:11 +0700

    Update lvl03_for02.c

--- ngatruong/lvl03_for02.c
+++ ngatruong/lvl03_for02.c
@@ -1,15 +1,16 @@
+
 #include<stdio.h>
 
 void main()
 
 {
-
     int a,i;
+    printf("cac so nguyen ba chu so sao cho tong tam thua cua ba chu so hang tram, hang chuc, hang đon vi se bang so nguyen do\n");
 
     for(i=100;i<=999;i++)
 
         if(((i/100)*(i/100)*(i/100)+((i%100)/10)*((i%100)/10)*((i%100)/10)+((i%100)%10)*((i%100)%10)*((i%100)%10))==i)
 
-        printf("%d\n",i);
+        printf(" %d\n",i);
 
 }
